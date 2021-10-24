986baa73abb1f97bd56019f2fa154b377d0e462d
bitf17a512 <bitf17a512@pucit.edu.pk>
2021-10-25 00:40:30 +0500

    Java code build

--- app/src/main/java/com/example/program10/MainActivity.java
+++ app/src/main/java/com/example/program10/MainActivity.java
@@ -1,14 +1,8 @@
-package com.example.program10;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+import androidx.appcompat.app.AppCompatActivity;
