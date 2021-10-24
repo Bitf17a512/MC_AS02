e537a742854b41cdef9efb5cdbee16b5a9f8d629
bitf17a512 <bitf17a512@pucit.edu.pk>
2021-10-25 00:49:14 +0500

    Given character is a grass Character

--- app/src/main/java/com/example/program10/MainActivity.java
+++ app/src/main/java/com/example/program10/MainActivity.java
@@ -83,3 +83,12 @@ public class MainActivity extends AppCompatActivity {
                                     text.setText("Given character is a grass Character");
 
                                 }
+                                else
+                                {
+                                    int comparer = Character.compare(char1, 'r');
+                                    if (comparer == 0)
+                                    {
+                                        TextView text = (TextView) findViewById(R.id.textView2);
+                                        text.setText("Given character is a grass Character");
+
+                                    }
