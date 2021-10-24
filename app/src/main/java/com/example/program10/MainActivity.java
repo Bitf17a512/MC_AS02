819aa3204e3ba3f9905e3ac7164806b8167d9d58
bitf17a512 <bitf17a512@pucit.edu.pk>
2021-10-25 00:44:33 +0500

    Condition within condition

--- app/src/main/java/com/example/program10/MainActivity.java
+++ app/src/main/java/com/example/program10/MainActivity.java
@@ -21,55 +21,7 @@ public class MainActivity extends AppCompatActivity {
         String strusername = medittext.getText().toString();
         Character char1 = strusername.charAt(0);
         //  System.out.println("abcd");
-        int cpackage com.example.myapplication;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-        public class MainActivity extends AppCompatActivity {
-
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-            }
-
-
-            public void sendMessage(View view) {
-                EditText medittext = (EditText) findViewById(R.id.editText1);
-                String strusername = medittext.getText().toString();
-                Character char1 = strusername.charAt(0);
-                //  System.out.println("abcd");
-                int compareOneTwo = Character.compare(char1, 'a');
-                if (compareOneTwo == 0)
-                {
-                    TextView text = (TextView) findViewById(R.id.textView2);
-                    text.setText("Given character is a grass Character");
-
-                }
-
-                else
-                {
-                    int comparec = Character.compare(char1, 'c');
-                    if (comparec == 0)
-                    {
-                        TextView text = (TextView) findViewById(R.id.textView2);
-                        text.setText("Given character is a grass Character");
-
-                    }
-                    else
-                    {
-                        int comparee = Character.compare(char1, 'e');
-                        if (comparee == 0)
-                        {
-                            TextView text = (TextView) findViewById(R.id.textView2);
-                            text.setText("Given character is a grass Character");
-
-                        }ompareOneTwo = Character.compare(char1, 'a');
+        int compareOneTwo = Character.compare(char1, 'a');
         if (compareOneTwo == 0)
         {
             TextView text = (TextView) findViewById(R.id.textView2);
@@ -86,3 +38,21 @@ import androidx.appcompat.app.AppCompatActivity;
                 text.setText("Given character is a grass Character");
 
             }
+            else
+            {
+                int comparee = Character.compare(char1, 'e');
+                if (comparee == 0)
+                {
+                    TextView text = (TextView) findViewById(R.id.textView2);
+                    text.setText("Given character is a grass Character");
+
+                }
+                else
+                {
+                    int comparei = Character.compare(char1, 'i');
+                    if (comparei == 0)
+                    {
+                        TextView text = (TextView) findViewById(R.id.textView2);
+                        text.setText("Given character is a grass Character");
+
+                    }
